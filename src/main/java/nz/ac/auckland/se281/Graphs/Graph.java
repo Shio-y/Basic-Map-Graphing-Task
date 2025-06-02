b8857a88082edfb1637ce9e4136edd250c46e810
Jie Xin Huang <jhua455@aucklanduni.ac.nz>
2025-06-03 01:25:03 +1200

    getShortestRoute now returns the shortest path as a list

--- src/main/java/nz/ac/auckland/se281/Graphs/Graph.java
+++ src/main/java/nz/ac/auckland/se281/Graphs/Graph.java
@@ -1,6 +1,7 @@
 package nz.ac.auckland.se281.Graphs;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -28,22 +29,34 @@ public class Graph {
   }
 
   public List<Node> getShortestRoute(Node root, Node target) {
+    Map<Node,Node> parentMap = new HashMap<>();
     List<Node> visited = new ArrayList<>();
     Queue<Node> queue = new LinkedList<>();
+    List<Node> path = new LinkedList<>();
+
+    parentMap.put(root, null);
     queue.add(root);
     visited.add(root);
     //BFS implementation
     while (!queue.isEmpty()) {
       Node currentNode = queue.poll();
+
       //ends the loop when target found 
       if (currentNode.equals(target)){
-        return visited;
+        //goes backwards through parentMap to find the path you took
+        for (Node n = target ; n != null ; n = parentMap.get(n)){
+            path.add(n);
+
+        }
+        Collections.reverse(path);
+        return path;
       }
       //checks if we have visited the adjacent nodes.
       for (Node nextNode : adjNodes.get(currentNode)) {
         if (!visited.contains(nextNode)) {
           visited.add(nextNode);
           queue.add(nextNode);
+          parentMap.put(nextNode, currentNode); //for making path later
         }
       }
     }
